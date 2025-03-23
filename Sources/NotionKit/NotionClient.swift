99fc8111dad7f4bdf56e0b43d4d478fc3dd93b94
Noah Moller <noah@tetrix.tech>
2025-03-23 17:02:09 +1000

    Updated so NotionKit works on non macos builds of swift
    Updated so NotionKit works on non macos builds of swift

--- Sources/NotionKit/NotionClient.swift
+++ Sources/NotionKit/NotionClient.swift
@@ -3,6 +3,23 @@ import Foundation
 import FoundationNetworking
 #endif
 
+extension URLSession {
+    func data(for request: URLRequest) async throws -> (Data, URLResponse) {
+        return try await withCheckedThrowingContinuation { continuation in
+            let task = self.dataTask(with: request) { data, response, error in
+                if let error = error {
+                    continuation.resume(throwing: error)
+                } else if let data = data, let response = response {
+                    continuation.resume(returning: (data, response))
+                } else {
+                    continuation.resume(throwing: URLError(.badServerResponse))
+                }
+            }
+            task.resume()
+        }
+    }
+}
+
 /// Implementation of the Notion API client
 public class NotionClient: NotionClientProtocol, @unchecked Sendable {
     // MARK: - Properties
